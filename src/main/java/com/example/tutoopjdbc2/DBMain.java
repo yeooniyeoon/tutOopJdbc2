d401ccc2873fc4168931f17de5d90ae0c9840e18
ycyeon <ickite@naver.com>
2023-05-31 14:45:06 +0900

    DBMain에 있던 DAO 객체를 클래스로 분리(리팩토링)

--- src/main/java/com/example/tutoopjdbc2/DBMain.java
+++ src/main/java/com/example/tutoopjdbc2/DBMain.java
@@ -2,6 +2,7 @@ package com.example.tutoopjdbc2;
 
 import com.example.tutoopjdbc2.model.BaseDAO;
 import com.example.tutoopjdbc2.model.Person;
+import com.example.tutoopjdbc2.model.PersonDAO;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -10,108 +11,12 @@ import java.util.Optional;
 
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
-        DBMain dbMain = new DBMain();
-        dbMain.initPerson();
-        System.out.println(dbMain.insertPerson("leo"));
-        System.out.println(dbMain.insertPerson("yui"));
-        List<Person> personsList = dbMain.findAllPerson();
+        PersonDAO personDAO = new PersonDAO();
+        personDAO.initPerson();
+        System.out.println(personDAO.insertPerson("leo"));
+        System.out.println(personDAO.insertPerson("yui"));
+        List<Person> personsList = personDAO.findAllPerson();
         System.out.println(personsList.toString());
-        System.out.println(dbMain.findByNamePerson("bab"));
-    }
-
-    private Optional<Person> findByNamePerson(String pname) {
-        String sql = "select id, name from person where name =?";
-        try {
-            getConn();
-            psmt = conn.prepareStatement(sql);
-            psmt.setString(1, pname);
-            rs = psmt.executeQuery();
-            if ( rs.next() ) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                return Optional.of(new Person(id, name));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close();
-        }
-        return Optional.empty();
-    }
-
-    private List<Person> findAllPerson() {
-        List<Person> result = new ArrayList<>();
-        String sql = "select id, name from person";
-        try {
-            getConn();
-            psmt = conn.prepareStatement(sql);
-            rs = psmt.executeQuery();
-            while( rs.next() ) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                result.add(new Person(id, name));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close();
-        }
-        return result;
-    }
-
-    private int insertPerson(String name) {
-        int cnt = 0;
-        String sql = "insert into person(name) values(?)";
-        try {
-            getConn();
-            psmt = conn.prepareStatement(sql);
-            psmt.setString(1, name);
-            cnt = psmt.executeUpdate();
-        }
-        catch(SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        finally {
-            close();
-        }
-        return cnt;
-    }
-
-    private void initPerson() {
-        String sql = """
-                create table person (
-                id integer primary key autoincrement,
-                name string
-                )
-                """;
-        try {
-            getConn();
-            smt = conn.createStatement();
-            smt.executeUpdate("drop table if exists person");
-            smt.executeUpdate(sql);
-        }
-        catch(SQLException e) {
-            System.err.println(e.getMessage());
-        }
-        finally {
-            close();
-        }
-    }
-
-    private void gatCodeName() {
-        String sql = "select code, name from country order by code, name";
-        try {
-            getConn();
-            psmt = conn.prepareStatement(sql);
-            rs = psmt.executeQuery();
-            while( rs.next() ) {
-                System.out.print( rs.getString("code") + "\t");
-                System.out.println( rs.getString("name"));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            close();
-        }
+        System.out.println(personDAO.findByNamePerson("bab"));
     }
 }
