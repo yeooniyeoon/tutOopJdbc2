4e1d6b3c27c3f49487c493df679796618ee43305
ycyeon <ickite@naver.com>
2023-05-31 14:20:27 +0900

    Person 전체 조회

--- src/main/java/com/example/tutoopjdbc2/DBMain.java
+++ src/main/java/com/example/tutoopjdbc2/DBMain.java
@@ -1,16 +1,42 @@
 package com.example.tutoopjdbc2;
 
 import com.example.tutoopjdbc2.model.BaseDAO;
+import com.example.tutoopjdbc2.model.Person;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBMain extends BaseDAO {
     public static void main(String[] args) {
         DBMain dbMain = new DBMain();
         dbMain.initPerson();
-        dbMain.insertPerson("leo");
-        dbMain.insertPerson("yui");
+        System.out.println(dbMain.insertPerson("leo"));
+        System.out.println(dbMain.insertPerson("yui"));
+        List<Person> personsList = dbMain.findAllPerson();
+        System.out.println(personsList.toString());
     }
+
+    private List<Person> findAllPerson() {
+        List<Person> result = new ArrayList<>();
+        String sql = "select id, name from person";
+        try {
+            getConn();
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while( rs.next() ) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                result.add(new Person(id, name));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
+        }
+        return result;
+    }
+
     private int insertPerson(String name) {
         int cnt = 0;
         String sql = "insert into person(name) values(?)";
